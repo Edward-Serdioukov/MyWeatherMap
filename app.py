f7d8309b6a8d53230593bf31938fdb661c3587b7
edward-serdioukov <edward.serdioukov@gmail.com>
2024-11-18 08:39:09 +0100

    week

--- app.py
+++ app.py
@@ -94,6 +94,46 @@ def get_weather_week(city_name):
     else:
         return None
 
+def get_weather_cities_week(city_name):
+    api_key = "6a9eedc6772205944011d6cbf0b0323f"
+    geocoding_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={api_key}"
+    geo_response = requests.get(geocoding_url)
+
+    if geo_response.status_code == 200:
+        geo_data = geo_response.json()
+        if len(geo_data) == 0:
+            return None
+        
+        lat = geo_data[0]['lat']
+        lon = geo_data[0]['lon']
+        
+        weather_url = f"http://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,alerts&appid={api_key}"
+        response = requests.get(weather_url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            daily_forecast = []
+            
+            for day in data['daily']:
+                # Преобразование метки времени в день недели
+                day_name = datetime.datetime.fromtimestamp(day['dt']).strftime('%A')
+                
+                daily_forecast.append({
+                    'day': day_name,  # Передаем уже отформатированное имя дня
+                    'temperature': day['temp']['day'] - 273.15,
+                    'pressure': day['pressure'] * 0.75006375541921,
+                    'humidity': day['humidity'],
+                    'wind_speed': day['wind_speed'],
+                    'description': day['weather'][0]['description'],
+                    'icon': f"http://openweathermap.org/img/wn/{day['weather'][0]['icon']}@2x.png"
+                })
+            
+            return {
+                'city': city_name,
+                'forecast': daily_forecast
+            }
+    return None
+
 @app.route('/sw.js')
 def serve_sw():
     return send_file('sw.js', mimetype='application/javascript')
@@ -102,8 +142,70 @@ def serve_sw():
 def serve_manifest():
     return send_file('manifest.json', mimetype='application/manifest+json')
 
+
 @app.route('/')
 def index():
+    folium_map = folium.Map(location=[48.0, 16.0], zoom_start=5)
+    folium_map.get_root().header.add_child(folium.CssLink('css/style.css'))
+    
+    for city in cities:
+        # Получаем данные о погоде
+        ###weather_today, weather_week = get_weather(city['lat'], city['lon'])
+        #weather_today = get_weather_day(city['name'])
+        # Создаем всплывающее окно с прогнозом на неделю
+        #popup_html += f"<img src='{weather_today['icon']}'><br>"
+        #popup_html += f"{weather_today['temperature']}°C, {weather_today['description']}<br>"
+        
+        weather_day = get_weather_day(city['name'])
+        if weather_day:
+            # Начинаем строить HTML для попапа
+            popup_html = f"<b>{city['name']}</b><br>"
+            popup_html += "<table border='1' style='width: 100%; border-collapse: collapse;'>"
+            
+            # Добавляем три столбца, каждый из которых будет содержать три строки с данными
+            popup_html += "<tr>"
+            popup_html += "<td style='padding: 10px; vertical-align: top;'>"
+                
+                # В каждом столбце — три строки (по одному дню)
+            
+            popup_html += f"<div style='margin-bottom: 10px;'>"
+            popup_html += f"<b>{weather_day['day']}</b><br>"
+            popup_html += f"<img src='{weather_day['icon']}'><br>"
+            popup_html += f"<b>{round(weather_day['temperature'], 1)}°C</b><br>"
+            popup_html += f"<i>{weather_day['description']}</i><br>"
+            popup_html += "</div>"
+                
+            popup_html += "</td>"
+
+            popup_html += "</tr></table>"
+        
+        # Добавляем маркер на карту
+        folium.Marker(
+            location=(city['lat'], city['lon']),
+            popup=popup_html,
+            #icon=folium.DivIcon(html=f"""<div style="font-family: courier new; color: red; font-weight: bold">{city['name']}, {weather_today['temperature']}°C</div>"""),
+            tooltip=f"{city['name']}: {weather_day['day']}, {round(weather_day['temperature'])}°C, {weather_day['description']}"
+        ).add_to(folium_map)
+        
+
+    folium_map.save('static/map.html')
+    return render_template('index.html',  title="My Weather Map", map_url='map.html')
+
+@app.route('/cities-week', methods=['GET'])
+def cities_week():
+    #cities = ['Milan', 'Venezia', 'Prague', 'Marianske Lazne']
+    cities_weather = []
+    
+    for city in cities:
+        weather_data = get_weather_cities_week(city['name'])
+        if weather_data:
+            cities_weather.append(weather_data)
+    
+    return render_template('cities_week.html', title="My Weather Week", cities_weather=cities_weather)
+
+
+@app.route('/index_week')
+def index_week():
     folium_map = folium.Map(location=[48.0, 16.0], zoom_start=5)
     folium_map.get_root().header.add_child(folium.CssLink('css/style.css'))
     
@@ -155,8 +257,6 @@ def index():
 
     folium_map.save('static/map.html')
     return render_template('index.html',  title="My Weather Map", map_url='map.html')
-
-
 
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
